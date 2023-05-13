c6aafafc2ff3840e81db6fae89f24e9288fd2312
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-05-13 14:34:18 +0300

    add handling rejected promises

--- script.js
+++ script.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${+(
+              data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+        `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
 //////////////////////////////////////
 /*
 const getCountryData = function (country) {
@@ -38,28 +64,6 @@ const getCountryData = function (country) {
 
 getCountryData('bulgaria');
 getCountryData('usa');
-*/
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${+(
-              data.population / 1000000
-            ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-        </article>
-        `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-/*
 
 const getCountryAndNeighbour = function (country) {
   // AJAX call country 1
@@ -125,8 +129,8 @@ setTimeout(() => {
 //     });
 // };
 
-const request = fetch('https://restcountries.com/v2/name/portugal');
-console.log(request);
+// const request = fetch('https://restcountries.com/v2/name/portugal');
+// console.log(request);
 
 const getCountryData = function (country) {
   // Country 1
@@ -142,6 +146,21 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, "neighbour"));
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 😢`);
+      renderError(`Something went wrong 😢${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
 };
-getCountryData('bulgaria');
+
+btn.addEventListener('click', function () {
+  getCountryData('bulgaria');
+});
+
+getCountryData('dsdas');
+
+
+
